1cbc81bd4c760dd91c2232ab8bc3c56ac6268541
Rhea Rivera <rjuner@gmail.com>
2016-05-19 21:35:37 -0500

    making sure my commit messages are correctly associated with this comp

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,28 +1,44 @@
 $( document ).ready(function() {
 
-//Establishing Characters: 
+//Establishing variables: 
 
 var charactersObj = [
 	{
 	name:"steven", 
-	attack: 10
+	hp: 100,
+	attack: 10,
+	counterAttack: 5,
+	images: "assets/images/steven.png"
 	},
 
 	{
 	name: "amethyst", 
-	attack: 20
+	hp: 100,
+	attack: 20, 
+	counterAttack: 10,
+	images: "assets/images/steven.png"
 	}, 
 
 	{
 	name: "pearl",
-	attack: 30, 
+	hp: 100,
+	attack: 30,
+	counterAttack: 15,
+	images: "assets/images/steven.png"
+
 	}, 
 
 	{
 	name: "garnet", 
+	hp: 100,
 	attack: 40, 
+	counterAttack: 20,
+	images: "assets/images/steven.png",
 	}
-]
+];
+
+var heroSelected = false; 
+var enemySelected = false; 
 
 
 //CLICK FUNCTIONS
@@ -33,12 +49,33 @@ for (var i = 0; i < charactersObj.length; i++) {
 		    var b = $('<button>');
 		    b.addClass('letter-button letter letter-button-color');
 		    console.log(b);
-		    b.attr('data-let', letters[i]);
-		    b.text(letters[i])
-
+		    b.attr('data-char', charactersObj[i].name);
+		    b.text(charactersObj[i].name);
 		    //alternatively you could do it in one line but it'll be harder to read
 		    //var b = $('<button class="letter-button letter letter-button-color" data-let="'+ letters[i] +'">').text(letters[i]);   
 		    
-		    $("#buttons").append(b); 
+		    $("#heroSelect").append(b); 
 		    //alert('watch this');
 		};
+
+	$('.letter-button').on('click', function() {
+	    	var clone = $(this).clone();
+	    	console.log(this.toString());
+
+			$("#hero").append(this);
+			//alert('watch this')
+	});
+
+
+	$('.letter-button').on('click', function() {
+		    	var clone = $(this).clone();
+		    	console.log(this.toString());
+
+				$("#enemy").append(this);
+				//alert('watch this')
+	});
+
+});
+
+
+
